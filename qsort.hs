e4ce71820629da5a3b09b57c2376459464f19b1c
shigemk2 <i.am.shige@gmail.com>
2014-09-21 17:32:21 +0900

    Add comment

--- qsort.hs
+++ qsort.hs
@@ -1,3 +1,4 @@
+-- リストの最初の要素4を基準に3つの部分に分けます
 qsort []     = []
 qsort (n:xs) = qsort lt ++ [n] ++ qsort gteq
     where
@@ -5,4 +6,4 @@ qsort (n:xs) = qsort lt ++ [n] ++ qsort gteq
         gteq = [x | x <- xs, x >= n]
 
 main = do
-    print $ qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
+    print $ qsort [11, 4, 6, 9, 8, 3, 5, 1, 7, 2]
