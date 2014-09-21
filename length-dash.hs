9fd131cceca63654ac784aaa5527a24dac1f711f
shigemk2 <i.am.shige@gmail.com>
2014-09-21 14:56:18 +0900

    Take

--- length-dash.hs
+++ length-dash.hs
@@ -1,11 +1,11 @@
 length' [] = 0
 length'(_:xs) = 1 + length' xs
 
--- take' n [] = []
--- take' n (x:xs) = 1 + take' xs
+take' 0 xs = []
+take' n xs = take' (n-1) xs ++ [n]
 
--- drop' n [] = []
--- drop' n (x:xs) = 1 + drop' xs
+drop' 0 xs = []
+drop' n (x:xs) = []
 
 reverse'[] = []
 reverse'(x:xs) = reverse' xs ++ [x]
@@ -18,7 +18,7 @@ product'(x:xs) = x * product' xs
 
 main = do
   print $ length' [1, 2, 3] -- 長さ
-  -- print $ take' 2 [1, 2, 3] -- 最初n個
+  print $ take' 1 [1, 2, 3] -- 最初n個
   -- print $ drop' 2 [1, 2, 3] -- 最初n個を落とす
   print $ reverse' [1, 2, 3, 4, 5] -- 逆に並べる
   print $ sum' [1, 2, 3, 4, 5] -- 全部たす
