58c7f52d56e3f990d907c2105fe544e3523f6508
shigemk2 <i.am.shige@gmail.com>
2014-10-26 02:00:42 +0900

    dropとtakeでごにょごにょする

--- my-merge.hs
+++ my-merge.hs
@@ -1,6 +1,12 @@
 msplit x =
-    take key x
+    -- take key x
+    drop key x
     where
+      m1 = take key x
+      m2 = drop key x
       key = (div (length x - 1) 2) + 1
 main = do
-  print $ msplit [1,2,3,4,5,6]
+  print $ msplit x
+  where
+    x = [1,2,3,4,5,6]
+
