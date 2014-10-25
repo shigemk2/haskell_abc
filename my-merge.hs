24abd45723ffa39b04d1bdaf374689f624fa1fac
shigemk2 <i.am.shige@gmail.com>
2014-10-26 02:15:27 +0900

    リストを右半分と左半分に分ける

--- my-merge.hs
+++ my-merge.hs
@@ -1,12 +1,14 @@
-msplit x =
-    -- take key x
+mtake x =
+    take key x
+    where
+      key = (div (length x - 1) 2) + 1
+mdrop x =
     drop key x
     where
-      m1 = take key x
-      m2 = drop key x
       key = (div (length x - 1) 2) + 1
 main = do
-  print $ msplit x
+  print $ mtake x
+  print $ mdrop x
   where
     x = [1,2,3,4,5,6]
 
