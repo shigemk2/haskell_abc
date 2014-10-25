63e2eda968454cca7ec61d4da5d45a785765dc22
shigemk2 <i.am.shige@gmail.com>
2014-10-25 17:22:44 +0900

    デバッグ情報の削除

--- bsort-debug2.hs
+++ bsort-debug2.hs
@@ -1,21 +1,10 @@
 import Debug.Trace
 
-bubble _ [x] = [x]
-bubble n (x:xs)
-    | y < x     = {- trace dbg2 $ -} y : bubble (n + 1) (x:ys)
-    | otherwise = {- trace dbg3 $ -} x : bubble (n + 1) (y:ys)
+bubble [x] = [x]
+bubble (x:xs)
+    | y < x     = y : bubble (x:ys)
+    | otherwise = x : bubble (y:ys)
     where
-      spc = replicate (n * 2) ' '
-      dbg1 = spc ++ "bubble (" ++ show x ++ ":" ++ show xs ++ ")"
-      dbg2 = spc ++ "= " ++ show y ++ " : bubble (" ++ show x ++ ":" ++ show ys ++ ")"
-      dbg3 = spc ++ "= " ++ show x ++ " : bubble (" ++ show y ++ ":" ++ show ys ++ ")"
-      (y:ys) = trace dbg1 $ bubble (n + 1) xs
-
--- bubble (x:xs) = x:y:ys
---     where
---       (y:ys) = bubble xs
-
+      (y:ys) = bubble xs
 main = do
-  -- print $ bubble 0 [10,2,4,5,1,3,6,9,8,7]
-  -- print $ bubble 0 [4, 3, 1, 5, 2]
-  print $ bubble 0 [3,8,15,14,1,2,4,9,13,7,6,11,5,8,10,12]
+  print $ bubble [3,8,15,14,1,2,4,9,13,7,6,11,5,8,10,12]
--- bsort-debug3.hs
+++ bsort-debug3.hs
@@ -1,24 +1,16 @@
 import Debug.Trace
 
-bubble _ [x] = [x]
-bubble n (x:xs)
+bubble [x] = [x]
+bubble (x:xs)
     | y < x     = y:x:ys
     | otherwise = x:y:ys
     where
-      spc = replicate (n * 2) ' '
-      dbg = spc ++ "bubble (" ++ show x ++ ":" ++ show xs ++ ")"
-      (y:ys) = trace dbg $ bubble (n + 1) xs
+      (y:ys) = bubble xs
 
-bsort _ [] = []
-bsort n xs = y : bsort (n + 1) ys
+bsort [] = []
+bsort xs = y : bsort ys
   where
-    spc = replicate (n * 2) ' '
-    dbg = spc ++ "bsort " ++ show xs
-    (y:ys) = trace dbg $ bubble n xs
+    (y:ys) = bubble xs
 
 main = do
-  -- print $ bubble [10,2,4,5,1,3,6,9,8,7]
-  -- print $ bubble [4, 3, 1, 5, 2]
-  -- print $ bsort 0 [10,2,4,5,1,3,6,9,8,7]
-  -- print $ bsort 0 [4, 3, 1, 5, 2]
-  print $ bsort 0 [3,8,15,14,1,2,4,9,13,7,6,11,5,8,10,12]
+  print $ bsort [3,8,15,14,1,2,4,9,13,7,6,11,5,8,10,12]
