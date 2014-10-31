8dab4cc547a0ac7a3d4a07d41425546915aa32cc
shigemk2 <i.am.shige@gmail.com>
2014-11-01 01:24:37 +0900

    デバッグとコメント

--- my-merge.hs
+++ my-merge.hs
@@ -1,11 +1,18 @@
+import Debug.Trace
+
+-- 復路で再帰関数
 merge xs [] = xs
 merge [] ys = ys
 merge (x:xs) (y:ys)
-      | x < y     = x : merge xs (y:ys)
-      | otherwise = y : merge (x:xs) ys
+      | x < y     = trace dbg1 $ x : merge xs (y:ys)
+      | otherwise = trace dbg2 $ y : merge (x:xs) ys
+      where
+        dbg1 = "x < y " ++ "x: " ++ show x ++ " y: " ++ show y ++ show (x:xs) ++ show (y:ys)
+        dbg2 = "otherwise " ++ show (x:xs) ++ show (y:ys)
 
 msort [] = []
 msort [x] = [x]
+-- 往路で分割
 msort xs = merge (msort (take h xs)) (msort (drop h xs))
            where
              h = (length xs) `div` 2
