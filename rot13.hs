48c84022559af520f4450e9ce3bcf9e116d3d40b
shigemk2 <i.am.shige@gmail.com>
2015-09-14 21:12:00 +0900

    ROT たぶん変

--- rot13.hs
+++ rot13.hs
@@ -6,3 +6,4 @@ main = do
     print [rot13 'A', rot13 'B', rot13 'C', rot13 'D', rot13 'E']
     print [rot13 x | x <- "abcde"]
     print [rot13 x | x <- "I am shigemk2"]
+    print [rot13 x | x <- "z"]
